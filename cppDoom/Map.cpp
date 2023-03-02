9b392e4f17909dc14b7a0a5312ec130b20ea7fb3
Kristof Sarkozi <krisi.sarkozi@gmail.com>
2023-03-02 23:57:36 +0100

    Node02 Completed working

--- cppDoom/Map.cpp
+++ cppDoom/Map.cpp
@@ -1,1 +1,17 @@
 #include "Map.h"
+
+Map::Map(std::string sName) : m_sName(sName) {}
+
+Map::~Map(){}
+
+void Map::AddVertex(Vertex& v) {
+	m_Vertexes.push_back(v);
+}
+
+void Map::AddLinedef(Linedef& l) {
+	m_Linedef.push_back(l);
+}
+
+std::string Map::GetName() {
+	return m_sName;
+}
